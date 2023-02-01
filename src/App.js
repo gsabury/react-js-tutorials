8baa36aeb1952f376e44a6bc37d685ff37041873
gsabury <gh.sabury@gmail.com>
2023-02-01 19:41:20 +0430

    Props example created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Hello from './Components/Hello';
+import Profile from './Components/Profile';
 function App() {
   return (
     <div className="App">
       <Hello />
+      <Profile fname="Ghafor" lname="Sabury" age="32"/>
+      <Profile fname="Jumah" lname="Mohammadi" age="30"/>
+      <Profile fname="Mahmood" lname="Sabeghi" age="29">
+        <p>Component Child</p>
+      </Profile>
     </div>
   );
 }
