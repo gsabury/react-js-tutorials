13957eca6291f5cc487395d9285afa3b1dcb6974
gsabury <gh.sabury@gmail.com>
2023-02-01 14:46:52 +0430

    Functional Component Example Created

---
+++ src/Components/Hello.js
@@ -0,0 +1,16 @@
+// Traditonal Syntax
+/*
+function Hello(){
+    return(
+        <h1>Hello Component </h1>
+    );
+}
+*/
+
+// ES6 Syntax
+const Hello = ()=>{
+    return(
+        <h1>Hello Component </h1>
+    );
+}
+export default Hello;
